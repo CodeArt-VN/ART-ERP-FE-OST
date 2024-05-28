f07a10c2929263d816f117d379ae5dd2b5df6360
Hùng Vũ <hungvq@live.com>
2024-05-28 09:31:04 +0700

    clean

--- routing.module.ts
+++ routing.module.ts
@@ -6,7 +6,7 @@ export const OSTRoutes: Routes = [
     { path: 'branch', loadChildren: () => import('./branch/branch.module').then(m => m.BranchPageModule), canActivate: [AuthGuard] },
     { path: 'branch/:id', loadChildren: () => import('./branch-detail/branch-detail.module').then(m => m.BranchDetailPageModule), canActivate: [AuthGuard] },
 
-    { path: 'office', loadChildren: () => import('./office/office.module').then(m => m.OfficePageModule), canActivate: [AuthGuard] },
-    { path: 'office/:id', loadChildren: () => import('./office-detail/office-detail.module').then(m => m.OfficeDetailPageModule), canActivate: [AuthGuard] },
+    // { path: 'office', loadChildren: () => import('./office/office.module').then(m => m.OfficePageModule), canActivate: [AuthGuard] },
+    // { path: 'office/:id', loadChildren: () => import('./office-detail/office-detail.module').then(m => m.OfficeDetailPageModule), canActivate: [AuthGuard] },
 
 ];
