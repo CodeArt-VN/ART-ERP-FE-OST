fd3ac6b9586915ce09ae4d3f8e80b26a35dac22b
tanphuc <bizyoh@gmail.com>
2025-06-20 09:15:05 +0700

    init

---
+++ value-chain/value-chain.module.ts
@@ -0,0 +1,13 @@
+import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ValueChainPage } from './value-chain.page';
+import { ShareModule } from 'src/app/share.module';
+
+@NgModule({
+	imports: [IonicModule, CommonModule, FormsModule, ShareModule, RouterModule.forChild([{ path: '', component: ValueChainPage }])],
+	declarations: [ValueChainPage],
+})
+export class ValueChainPageModule {}
